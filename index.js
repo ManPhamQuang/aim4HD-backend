97e95f4870505cf2892e4592899b340cf72c2644
Thang <dangvuthang97@gmail.com>
2020-11-11 09:32:27 +0700

    Create basic server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
-
+const userRouter = require("./route/userRoute");
+const skillRouter = require("./route/skillRoute");
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/skills", skillRouter);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const mongoose = require("mongoose");
+const app = require("./index");
+
+mongoose
+  .connect(
+    process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("CONNECT TO DATABASE"))
+  .catch(e => console.log(e));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
