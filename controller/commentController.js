a51015706079283a96fd0719d494c68bec26cc11
Thang <dangvuthang97@gmail.com>
2020-11-25 09:27:45 +0700

    CRUD for Comment

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,6 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 const Comment = require("../models/Comment");
 const ApiFeature = require("../utils/ApiFeature");
+const AppError = require("../utils/appError");
 
 exports.getAllComment = catchAsync(async (req, res, next) => {
   const currentQuery = Comment.find({ post: req.params.id })
@@ -29,4 +30,29 @@ exports.createComment = catchAsync(async (req, res, next) => {
       comment,
     },
   });
+});
+
+exports.updateComment = catchAsync(async (req, res, next) => {
+  const comment = await Comment.findByIdAndUpdate(
+    req.params.id,
+    {
+      content: req.body.content,
+    },
+    { new: true }
+  );
+  if (!comment)
+    return next(new AppError("No Comment found with a given ID", 404));
+  res.status(200).json({
+    status: "success",
+    data: {
+      comment,
+    },
+  });
+});
+
+exports.deleteComment = catchAsync(async (req, res, next) => {
+  const comment = await Comment.findByIdAndDelete(req.params.id);
+  if (!comment)
+    return next(new AppError("No Comment found with a given ID", 404));
+  res.status(204).end();
 });
--- route/commentRoute.js
+++ route/commentRoute.js
@@ -3,8 +3,12 @@ const router = express.Router({ mergeParams: true });
 const {
   getAllComment,
   createComment,
+  updateComment,
+  deleteComment,
 } = require("../controller/commentController");
 
 router.get("/", getAllComment);
 router.post("/", createComment);
+router.patch("/:id", updateComment);
+router.delete("/:id", deleteComment);
 module.exports = router;
