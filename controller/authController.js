e93a9234a609817d65fd81bcba4d36e8c3e39fbf
Vu Thang <dangvuthang97@gmail.com>
2020-12-21 10:04:31 +0700

    Manually send token

--- controller/authController.js
+++ controller/authController.js
@@ -6,48 +6,46 @@ const { promisify } = require("util");
 
 const createJWT = async id =>
   await promisify(jwt.sign)({ id }, process.env.JWT_SECRET, {
-    expiresIn: 1000 * 60 * 60 * process.env.JWT_EXPIRE,
+    expiresIn: 1000 * 60 * 60 * parseInt(process.env.JWT_EXPIRE),
   });
-
-const setCookieAndSendResponse = async (id, res, statusCode, data) => {
-  const token = await createJWT(id);
-  const option = {
-    expires: new Date(
-      Date.now() + 1000 * 60 * 60 * process.env.COOKIE_EXPIRES_IN
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") option.secure = true;
-  res.cookie("jwt", token, option);
-  res.status(statusCode).json(data);
-};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
-  return await setCookieAndSendResponse(user.id, res, 201, {
+  const token = await createJWT(user.id);
+  return res.status(200).json({
     status: "success",
-    data: { user },
+    data: {
+      user,
+      token,
+    },
   });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) return next(new AppError("No user found", 404));
-  return await setCookieAndSendResponse(user.id, res, 200, {
+  const token = await createJWT(user.id);
+  return res.status(200).json({
     status: "success",
-    data: { user },
+    data: {
+      user,
+      token,
+    },
   });
 });
 
-exports.checkLogin = catchAsync(async (req, res, next) => {
-  if (!req.cookies.jwt)
-    return next(new AppError("Please login to access this route", 400));
-  const decoded = await promisify(jwt.verify)(
-    req.cookies.jwt,
-    process.env.JWT_SECRET
-  );
+exports.checkUser = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  )
+    token = req.headers.authorization.split(" ")[1];
+  if (!token)
+    return next(new AppError("Please login to access this route", 401));
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   const user = await User.findById(decoded.id);
-  if (!user) return next(new AppError("User not found with a given ID", 400));
+  if (!token) return next(new AppError("User not found", 404));
   req.user = user;
   next();
 });
@@ -55,15 +53,14 @@ exports.checkLogin = catchAsync(async (req, res, next) => {
 exports.checkIfLoginWithMicrosoft = catchAsync(async (req, res, next) => {
   const user = await User.findOne({
     microsoftUniqueId: req.body.microsoftUniqueId,
-  })
-    .populate("skills")
-    .populate("currentCourses")
-    .populate("interestedPosts");
+  });
   if (!user) return next(new AppError("User not exist", 404));
+  const token = await createJWT(user.id);
   res.status(200).json({
     status: "success",
     data: {
       user,
+      token,
     },
   });
 });
