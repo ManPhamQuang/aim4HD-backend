ae2895c73ff7ff0174f9c4ede95d95eddb4ede86
Vu Thang <dangvuthang97@gmail.com>
2021-01-04 09:51:10 +0700

    Add send email functionality

---
+++ utils/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async options => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+      ciphers: "SSLv3",
+    },
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: `Aim4HD Team <${process.env.USER}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  return await transporter.sendMail(mailOptions);
+};
