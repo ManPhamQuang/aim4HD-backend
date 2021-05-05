e72f6815a698816010f8ef9a62bb2eba2d744158
NeoTheSecond <quangman1404@gmail.com>
2021-05-05 12:07:28 +0700

    populate skills and currentCourse when login

--- controller/authController.js
+++ controller/authController.js
@@ -22,7 +22,9 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email: req.body.email })
+        .populate("skills")
+        .populate("currentCourses");
     if (!user) return next(new AppError("No user was found", 404));
     const token = await createJWT(user.id);
     return res.status(200).json({
@@ -53,7 +55,9 @@ exports.checkUser = catchAsync(async (req, res, next) => {
 exports.checkIfLoginWithMicrosoft = catchAsync(async (req, res, next) => {
     const user = await User.findOne({
         microsoftUniqueId: req.body.microsoftUniqueId,
-    });
+    })
+        .populate("skills")
+        .populate("currentCourses");
     if (!user) return next(new AppError("User not exist", 404));
     const token = await createJWT(user.id);
     res.status(200).json({
